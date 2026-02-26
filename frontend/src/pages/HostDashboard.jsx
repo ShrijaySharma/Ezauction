9b35761a7af7a5b6d7e1176596535dcb5712dd20
ShrijaySharma <kanhasharma313@gmail.com>
2026-02-26 12:48:08 +0530

    Reduce size of EzAuction logo and move it further left

--- frontend/src/pages/HostDashboard.jsx
+++ frontend/src/pages/HostDashboard.jsx
@@ -243,8 +243,8 @@ function HostDashboard({ user }) {
       <div className="relative z-10 h-full w-full flex flex-col">
         {/* Top bar - Simplified & Centered */}
         <div className="h-32 flex items-center justify-end px-8 bg-black/40 backdrop-blur-md border-b border-white/10 relative">
-          <div className="absolute left-6 lg:left-12 top-1/2 -translate-y-1/2 flex flex-col items-center">
-            <img src="/ezauction.png" alt="EzAuction Logo" className="h-16 sm:h-20 lg:h-28 object-contain drop-shadow-[0_0_30px_rgba(255,255,255,0.2)] hover:scale-105 transition-transform duration-500" />
+          <div className="absolute left-4 lg:left-8 top-1/2 -translate-y-1/2 flex flex-col items-center">
+            <img src="/ezauction.png" alt="EzAuction Logo" className="h-10 sm:h-12 lg:h-16 object-contain drop-shadow-[0_0_30px_rgba(255,255,255,0.2)] hover:scale-105 transition-transform duration-500" />
           </div>
 
           <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
