42692350d5acdd9e61c34d91c036b039d3f55aad
ShrijaySharma <kanhasharma313@gmail.com>
2026-02-26 12:45:43 +0530

    Fix alignment of logout and sound buttons on Host dashboard

--- frontend/src/pages/HostDashboard.jsx
+++ frontend/src/pages/HostDashboard.jsx
@@ -242,7 +242,7 @@ function HostDashboard({ user }) {
 
       <div className="relative z-10 h-full w-full flex flex-col">
         {/* Top bar - Simplified & Centered */}
-        <div className="h-32 flex items-center justify-between px-8 bg-black/40 backdrop-blur-md border-b border-white/10 relative">
+        <div className="h-32 flex items-center justify-end px-8 bg-black/40 backdrop-blur-md border-b border-white/10 relative">
           <div className="absolute left-6 lg:left-12 top-1/2 -translate-y-1/2 flex flex-col items-center">
             <img src="/ezauction.png" alt="EzAuction Logo" className="h-16 sm:h-20 lg:h-28 object-contain drop-shadow-[0_0_30px_rgba(255,255,255,0.2)] hover:scale-105 transition-transform duration-500" />
           </div>
